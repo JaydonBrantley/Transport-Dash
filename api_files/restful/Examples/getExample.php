b2501eb0215336bca165dac9860255faa00a9d58
JaydonBrantley <jaydon.brantley@outlook.com>
2022-09-01 19:39:30 -0500

    Improved PHP examples to display correct syntax

---
+++ api_files/restful/Examples/getExample.php
@@ -0,0 +1,27 @@
+<?php
+
+// Credentials
+$dbhost = '';
+$dbhost = '';
+$dbhost = '';
+$dbhost = '';
+
+
+// 1. Create a database connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// 2. Perform database query
+$query = "SELECT * FROM subjects ";
+$result_set = mysqli_query($connection, $query);
+
+// 3. Use returned data
+
+
+// 4. Release returned data
+mysqli_free_result($result_set);
+
+// 5. Close database connection
+mysqli_close($connection);
+
+
+?>

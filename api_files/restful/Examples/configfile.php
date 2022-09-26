b44a487e7369a12ce04034a1a31f2f3cea284371
JaydonBrantley <jaydon.brantley@outlook.com>
2022-09-26 14:04:35 -0500

    Update configfile.php

--- api_files/restful/Examples/configfile.php
+++ api_files/restful/Examples/configfile.php
@@ -29,9 +29,51 @@ mysqli_close($connection);
 // Functions
 
 function getRoutes(){
-    global $connection;
     $query = "SELECT * FROM tblRoutes";
-    $array
+    $result_set = mysqli_query($connection, $query);
+    echo json_encode($result_set);
 }
+
+function getEmployees(){
+    $strQuery = "SELECT * FROM tblEmployees";
+    $arrEmployees = mysqli_query($connection, $strQuery);
+    echo json_encode($result_set);
+}
+
+function getVans(){
+    $strQuery = "SELECT * FROM tblVans";
+    $result_set = mysqli_query($connection, $strQuery);
+    echo json_encode($result_set);
+}
+
+function getStops(){
+    $strQuery = "SELECT * FROM tblStops";
+    $result_set = mysqli_query($connection, $strQuery);
+    echo json_encode($result_set);
+}
+
+function getAdmins(){
+    $strQuery = "SELECT * FROM tblAdmins";
+    $result_set = mysqli_query($connection, $strQuery);
+    echo json_encode($result_set);
+}
+
+function getCustomers(){
+    $strQuery = "SELECT * FROM tblCustomers";
+    $result_set = mysqli_query($connection, $strQuery);
+    echo json_encode($result_set);
+}
+
+
+function verifyAdmin($strEmpEmail, $strAdminPass){
+    $strQueryEmail = "SELECT Emp_Email FROM tblEmployees WHERE tblEmployees.Emp_Email = ? AND Admin_Password = ?";
+    $strQueryPassword = "SELECT Admin_Password FROM tblEmployees WHERE tblEmployees.Emp_Email = ? AND Admin_Password = ?";
+    $result_set_Email = mysqli_query($connection, $strQueryEmail);
+    $result_set_Pass = mysqli_query($connection, $strQueryPass);
+    if ($result_set_Email == $strEmpEmail and $result_set_Pass == $strAdminPass){
+        
+    }
+}
+
 
 ?>

88001d072bcf7fd92375b4c3e45d1b8d6d753dcc
JaydonBrantley <jaydon.brantley@outlook.com>
2022-10-05 13:07:26 -0500

    Updated config.php and added getCustomerInfo.php

---
+++ api_files/restful/getCustomerInfo.php
@@ -0,0 +1,7 @@
+<?php
+    require('config.php');
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $strCustomer = $_POST['Email'];
+        $strCustomer = $_POST['Password'];
+        $strUsername
+    }

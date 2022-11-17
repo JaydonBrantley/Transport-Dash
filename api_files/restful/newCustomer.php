71044585c5eb499f4bdb607974d1fe92e0204fe9
JaydonBrantley <jaydon.brantley@outlook.com>
2022-11-16 20:44:14 -0600

    Added Customer Signup Page

--- api_files/restful/newCustomer.php
+++ api_files/restful/newCustomer.php
@@ -13,19 +13,4 @@
 
         echo newCustomer($strCellNum, $strDiscountStat, $strSpecReq, $strAgeCat);
     }
-
-    if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-        $put_vars;
-        parse_str(file_get_contents("php://input"),$put_vars);
-        $intPrepaidRides = $put_vars['PrepaidRides'];
-        $strCellNum = $put_vars['CellNum'];
-
-        $intPrepaidRides = strip_tags($intPrepaidRides);
-        $strCellNum = strip_tags($strCellNum);
-        
-        echo updatePrepaidRides($intPrepaidRides, $strCellNum);
-
-    }
-
-
 ?>
